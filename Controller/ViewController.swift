58ed948ab76819b1247be0b72bad27f6221fae7a
Souiid <33631580+Souiid@users.noreply.github.com>
2018-06-12 17:21:08 +0200

    Add files via upload
    Add functions for: open pop up to share / swipe photoView / add photo in views /

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -6,21 +6,135 @@
 //  Copyright © 2018 Souissi idriss2. All rights reserved.
 //
 
+
+// Transformer photoview en image / deplacer outlet / refresh func, valid photoview
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
 
    
     @IBOutlet var layoutButtons: [UIButton]!
     @IBOutlet weak var photoView: PhotoView!
+    let imagePickerController = UIImagePickerController()
+    
+    @IBOutlet var photoButtons: [UIButton]!
+    @IBOutlet var images: [UIImageView]!
+    
+    @IBOutlet weak var stackView: UIStackView!
+   
+    var selectorIndex = 0
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        for button in layoutButtons {
+            button.isSelected = false
+        }
+
+        layoutButtons[1].isSelected = true
+        photoView.style = .upSquares
+        
+        let swipenGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(swipePhotoView(_:)))
+            swipenGestureRecognizer.direction = UISwipeGestureRecognizerDirection.up
+        photoView.addGestureRecognizer(swipenGestureRecognizer)
+        
+        
+        imagePickerController.delegate = self
         
     }
     
+    
+    
+    
+    
+    
+      func openPopUpToShare(){
+       let activityC = UIActivityViewController(activityItems: ["www.google.com"], applicationActivities: nil)
+
+      present(activityC, animated: true, completion: nil)
+        activityC.completionWithItemsHandler = { activity, completed, items, error in
+            //
+        }
+    }
+    
+    
+    
+    
+    
+    @objc private func swipePhotoView(_ gesture: UISwipeGestureRecognizer) {
+        switch gesture.state {
+        case .ended, .cancelled :
+            transformPhotoView(gesture: gesture)
+            openPopUpToShare()
+            photoView.refresh()
+        default:
+            break
+        }
+    }
+    
+    func moveUp(view: UIView){
+        view.center.y += 100
+    }
+    
+    private func transformPhotoView(gesture: UISwipeGestureRecognizer) {
+        let translation = gesture.location(in: photoView)
+      
+        
+        print("TRANSLATION: ", translation)
+        
+        UIView.animate(withDuration: 0.1, animations: {
+           self.photoView.center.y -= 40
+        })
+        
+    }
+    
+    
+    @IBAction func addPhoto(_ sender: UIButton) {
+        
+        let ind = photoButtons.index(of: sender)
+        selectorIndex = ind!
+        print(selectorIndex)
+        print("PHOTO BUTTON : ", selectorIndex)
+        
+        
+        
+        
+        imagePickerController.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePickerController.allowsEditing = false
+        
+        present(imagePickerController, animated: true) {
+        
+        
+        }
+        
+    }
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let myImage = images[selectorIndex]
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            myImage.image = image
+            photoButtons[selectorIndex].isHidden = true
+        }else{
+            print("AAAAA")
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+        
+        
     func modifyLayout(index: Int) {
+        
         switch index {
         case 0:
              photoView.style = .downSquares
@@ -39,6 +153,10 @@ class ViewController: UIViewController {
             button.isSelected = false
         }
         sender.isSelected = true
+        let ind = layoutButtons.index(of: sender)
+        print(ind!)
+        modifyLayout(index: ind!)
+        
         
     
     
